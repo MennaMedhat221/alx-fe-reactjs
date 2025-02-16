99a3343154b5530281b567d595d477b739804787
MennaMedhatt <menna.medhat@thefabulous.co>
2025-02-16 22:39:14 +0200

    maincontet2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Main from './components/MainContent'
+import MainContent from './components/MainContent'
 import './App.css'
 
 
