8b318999c79e55572dbef0f69033279c00353b17
MennaMedhatt <menna.medhat@thefabulous.co>
2025-02-16 22:37:31 +0200

    new content

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function Main() {
+    return(
+        <div>
+             <main>
+             <p>I love to visit New York, Paris, and Tokyo.</p>
+             </main>
+        </div>
+    
+    );
+}
+
+export default Main
