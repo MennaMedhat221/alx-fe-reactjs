a5363fd7e76b5571b591299c81d7d8dbab543634
MennaMedhatt <menna.medhat@thefabulous.co>
2025-02-16 22:44:44 +0200

    add prop

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 
@@ -15,6 +16,7 @@ function App() {
       <div>
         <Header />
         <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
       <WelcomeMessage />
       <Footer />
