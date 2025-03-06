2d00f95847dfab05f70ea84722f82286e4c7054d
MennaMedhatt <menna.medhat@thefabulous.co>
2025-03-06 10:43:49 +0200

    fix1

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MainContent from './components/MainContent'
-import ProfilePage from './components/ProfilePage';
-import UserContext from './UserContext';
-import './App.css'
+import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;
