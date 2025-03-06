9d2f02e44928c0c9d2fbc467a3057ac2663a6698
MennaMedhatt <menna.medhat@thefabulous.co>
2025-03-06 10:30:20 +0200

    dublicated dir

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+      <WelcomeMessage />
+      <Counter />
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
