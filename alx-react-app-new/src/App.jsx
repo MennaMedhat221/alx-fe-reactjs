fdee0f21f24ce00804a88814b846749bd6617d7b
MennaMedhatt <menna.medhat@thefabulous.co>
2025-03-06 10:41:40 +0200

    dublicated dir

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,26 +5,18 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
-import UserProfile from './components/UserProfile'
-import Counter from './components/Counter'
+import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
 import './App.css'
 
-
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-      <div>
-        <Header />
-        <MainContent />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      </div>
-      <WelcomeMessage />
-      <Counter />
-      <Footer />
-
-    </>
-  )
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
