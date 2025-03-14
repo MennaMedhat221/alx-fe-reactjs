c0f6bd5f240d8c06b38ff274b7552b81cc2cd442
MennaMedhatt <menna.medhat@thefabulous.co>
2025-03-15 01:01:09 +0200

    Set inline style2

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-      <nav className="navbar" style={{ backgroundColor: 'grey', textAlign: 'center' }}>
+      <nav className="navbar" style={{ backgroundColor: 'grey', textAlign: 'center' , display:'flex' , justifyContent:'center' }}>
         <ul className="nav-links">
           <li style={{ padding: '40px', textAlign: 'center' }}><Link to="/">Home</Link></li>
           <li style={{ padding: '40px', textAlign: 'center' }}><Link to="/about">About</Link></li>
