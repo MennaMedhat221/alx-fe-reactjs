a947c74355871d140c4d69eebc7833ec32a86b00
MennaMedhatt <menna.medhat@thefabulous.co>
2025-03-15 00:25:21 +0200

    fix4

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/home';
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -15,6 +18,13 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/Home" element={<Home />} />
+    <Route path="/About" element={<About />} />
+
+    </Routes>
+    </BrowserRouter>
       <div>
         <Header />
         <MainContent />
--- alx-react-app-props/src/components/InputName.jsx
+++ alx-react-app-props/src/components/InputName.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 
 function InputName() {
     const [details, setDetails] = useState({ name: "" });
