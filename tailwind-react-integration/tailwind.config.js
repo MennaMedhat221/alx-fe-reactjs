54eb923b9ec4cbffc9cc31648c8af450e7eb4908
MennaMedhatt <menna.medhat@thefabulous.co>
2025-03-24 11:56:37 +0200

    Add purge

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,6 @@
-export default {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
+module.exports = {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false,
     theme: { extend: {} },
     variants: { extend: {} },
     plugins: [],
